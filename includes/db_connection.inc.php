eb70397fc2005cf36e52042910449646604ce910
peter2707 <s3848409@student.rmit.edu.au>
2021-09-02 15:51:15 +1000

    config db again, fingers crossed

--- includes/db_connection.inc.php
+++ includes/db_connection.inc.php
@@ -1,12 +1,12 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("us-cdbr-east-04.cleardb.com"));
-$cleardb_server = $cleardb_url["80"];
-$cleardb_username = $cleardb_url["b562d2d71ed125"];
-$cleardb_password = $cleardb_url["5e707588"];
-$cleardb_db = substr($cleardb_url["heroku_4846e6f92797d07"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+	//Get Heroku ClearDB connection information
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+	// Connect to DB
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7fef75d079bd9192f12f5fbd213ab4968cd02c92',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7fef75d079bd9192f12f5fbd213ab4968cd02c92',
+            'dev_requirement' => false,
+        ),
+    ),
+);
