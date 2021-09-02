70b2882f721c2910019b7907ed94820e4ba3bd88
peter2707 <s3848409@student.rmit.edu.au>
2021-09-02 15:28:09 +1000

    config db again

--- includes/db_connection.inc.php
+++ includes/db_connection.inc.php
@@ -10,3 +10,16 @@
 		exit;
 	}
 ?>
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("us-cdbr-east-04.cleardb.com"));
+$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+$cleardb_username = $cleardb_url["b562d2d71ed125"];
+$cleardb_password = $cleardb_url["5e707588"];
+$cleardb_db = substr($cleardb_url["us-cdbr-east-04.cleardb.com"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
