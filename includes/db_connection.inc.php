7fef75d079bd9192f12f5fbd213ab4968cd02c92
peter2707 <s3848409@student.rmit.edu.au>
2021-09-02 15:33:24 +1000

    config db again

--- includes/db_connection.inc.php
+++ includes/db_connection.inc.php
@@ -1,23 +1,10 @@
 <?php
-	$dbAddress = 'localhost';
-	$dbUser = 'root';
-	$dbPass = '';
-	$dbName = 'heroku_4846e6f92797d07';
-
-	$db = new mysqli($dbAddress, $dbUser, $dbPass, $dbName);
-	if ($db->connect_error) {
-		echo "Could not connect to the database.  Please try again later.";
-		exit;
-	}
-?>
-
-<?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("us-cdbr-east-04.cleardb.com"));
-$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+$cleardb_server = $cleardb_url["80"];
 $cleardb_username = $cleardb_url["b562d2d71ed125"];
 $cleardb_password = $cleardb_url["5e707588"];
-$cleardb_db = substr($cleardb_url["us-cdbr-east-04.cleardb.com"],1);
+$cleardb_db = substr($cleardb_url["heroku_4846e6f92797d07"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
