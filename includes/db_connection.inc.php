3125770ce44b0384b7bafa382b9a2e0368ec61a4
peter2707 <s3848409@student.rmit.edu.au>
2021-09-14 23:52:05 +1000

    config db for heroku

--- includes/db_connection.inc.php
+++ includes/db_connection.inc.php
@@ -1,30 +1,30 @@
 
 <?php
-	// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	// $cleardb_server = $cleardb_url["host"];
-	// $cleardb_username = $cleardb_url["user"];
-	// $cleardb_password = $cleardb_url["pass"];
-	// $cleardb_db = substr($cleardb_url["path"],1);
-	// $active_group = 'default';
-	// $query_builder = TRUE;
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
 
-	// $db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-	// if ($db->connect_error) {
-	// 	echo "Could not connect to the database.  Please try again later.";
-	// 	exit;
-	// }
-?>
-
-
-<?php
-	$dbAddress = 'localhost';
-	$dbUser = 'root';
-	$dbPass = '';
-	$dbName = 'jobmatch';
-
-	$db = new mysqli($dbAddress, $dbUser, $dbPass, $dbName);
+	$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 	if ($db->connect_error) {
 		echo "Could not connect to the database.  Please try again later.";
 		exit;
 	}
+?>
+
+
+<?php
+	// $dbAddress = 'localhost';
+	// $dbUser = 'root';
+	// $dbPass = '';
+	// $dbName = 'jobmatch';
+
+	// $db = new mysqli($dbAddress, $dbUser, $dbPass, $dbName);
+	// if ($db->connect_error) {
+	// 	echo "Could not connect to the database.  Please try again later.";
+	// 	exit;
+	// }
 ?>
